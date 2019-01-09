59bbdf8284225711dd5cab79aebf518ca7a147b8
sohoorc <me@gwtcoder.me>
2019-01-09 22:32:55 +0800

    添加全局配置

---
+++ lib/global.config.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GlobalConfig {
+  static bool dark = true;
+  static ThemeData themeData = new ThemeData(
+    primarySwatch: Colors.red,
+  );
+  static Color searchBackgroundColor = Colors.white10;
+  static Color cardBackgroundColor = new Color(0xFF222222);
+  static Color fontColor = Colors.white30;
+}
