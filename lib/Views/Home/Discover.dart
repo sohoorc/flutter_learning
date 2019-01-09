dfffebc89bf5211ddf279916a027d69c2d4bf342
sohoorc <me@gwtcoder.me>
2019-01-09 21:47:50 +0800

    init

---
+++ lib/Views/Home/Discover.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Discover extends StatefulWidget {
+  _DiscoverState createState() => new _DiscoverState();
+}
+
+class _DiscoverState extends State<Discover> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        Text('发现先')
+      ],
+    );
+  }
+}
